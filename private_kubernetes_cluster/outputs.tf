360efbfdc592dcb82c49c9567b848e4d5a0bd040
Armin Aria <armin.aria@de.rhenus.com>
2022-05-09 15:15:40 +0200

    Added outputs for kubernetes

--- private_kubernetes_cluster/outputs.tf
+++ private_kubernetes_cluster/outputs.tf
@@ -1,3 +1,19 @@
 output "id" {
   value = azurerm_kubernetes_cluster.private-kubernetes.id
 }
+
+output "host" {
+  value = azurerm_kubernetes_cluster.private-kubernetes.kube_config[0].host
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.private-kubernetes.kube_config[0].client_key
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.private-kubernetes.kube_config[0].client_certificate
+}
+
+output "ca_certificate" {
+  value = azurerm_kubernetes_cluster.private-kubernetes.kube_config[0].cluster_ca_certificatey
+}
