95be49a4552e1be5277c222aa38900e1f55f1d18
Armin Aria <armin.aria@de.rhenus.com>
2022-05-09 11:06:06 +0200

    removed dependency on record set

--- private_key_vault/outputs.tf
+++ private_key_vault/outputs.tf
@@ -3,6 +3,5 @@ output "id" {
   depends_on = [
     azurerm_key_vault_access_policy.service-principal,
     azurerm_key_vault.key-vault,
-    azurerm_private_dns_a_record.endpoint-dns-a-record
   ]
 }
