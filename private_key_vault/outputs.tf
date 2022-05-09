c5c608884fb7b6137a80fd75b6afba5416e8254e
Armin Aria <armin.aria@de.rhenus.com>
2022-05-09 08:31:50 +0200

    Added dependency for the output

--- private_key_vault/outputs.tf
+++ private_key_vault/outputs.tf
@@ -1,3 +1,8 @@
 output "id" {
-  value = azurerm_key_vault.key-vault.id
+  value      = azurerm_key_vault.key-vault.id
+  depends_on = [
+    azurerm_key_vault_access_policy.service-principal,
+    azurerm_key_vault.key-vault,
+    azurerm_private_dns_a_record.endpoint-dns-a-record
+  ]
 }
