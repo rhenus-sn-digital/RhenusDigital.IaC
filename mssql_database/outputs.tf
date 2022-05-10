4109ccdb6988664e76ec8cef7e54799c9f18e534
Armin Aria <armin.aria@de.rhenus.com>
2022-05-10 10:08:37 +0200

    Added connection string as output

--- mssql_database/outputs.tf
+++ mssql_database/outputs.tf
@@ -6,3 +6,7 @@ output "password" {
   sensitive = true
   value     = mssql_login.login.password
 }
+
+output "connection_string" {
+  value = "Server=tcp:${var.sql_server_name}.database.windows.net,1433;Initial Catalog=${local.name};Persist Security Info=False;User ID=${mssql_user.user.login_name};Password=${mssql_login.login.password};MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+}
