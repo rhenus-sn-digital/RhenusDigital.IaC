2ae45f47d1f2e7259c20f27a9a4c800ff5822b47
Armin Aria <armin.aria@de.rhenus.com>
2022-05-10 11:56:03 +0200

    Added connection string to outputs

--- private_storage/outputs.tf
+++ private_storage/outputs.tf
@@ -5,3 +5,7 @@ output "name" {
 output "private-ip" {
   value = azurerm_private_endpoint.private-endpoint.private_service_connection[0].private_ip_address
 }
+
+output "connection_string" {
+  value = azurerm_storage_account.storage-account.primary_connection_string
+}
