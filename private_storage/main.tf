92245f8c4f6cd739df62b3d9e36acb5236917e4a
Armin Aria <armin.aria@de.rhenus.com>
2022-05-11 11:05:46 +0200

    azurerm cit

--- private_storage/main.tf
+++ private_storage/main.tf
@@ -2,22 +2,13 @@ terraform {
   required_version = "~>1.1.5"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.97.0"
+      source                = "hashicorp/azurerm"
+      version               = "~>2.97.0"
+      configuration_aliases = [default, azurerm.cit]
     }
   }
 }
 
 locals {
   name = "st${var.organization}${var.domain}${var.application}${var.stage}"
-}
-
-provider "azurerm" {
-  alias                      = "cit"
-  skip_provider_registration = true
-  subscription_id            = var.cit_subscription_id
-  tenant_id                  = var.cit_tenant_id
-  client_id                  = var.sp_client_id
-  client_secret              = var.sp_client_secret
-  features {}
 }
