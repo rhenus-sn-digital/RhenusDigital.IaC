cc9665aa47263464f6fbb3fcdb5b9466d59d1a53
Armin Aria <armin.aria@de.rhenus.com>
2022-05-18 09:44:18 +0200

    updated parameters

--- private_storage/storage.tf
+++ private_storage/storage.tf
@@ -1,14 +1,14 @@
 ## Storage Account
 
 resource "azurerm_storage_account" "storage-account" {
-  name                     = local.name
-  location                 = var.location
-  resource_group_name      = var.resource_group_name
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  min_tls_version          = var.min_tls_version
-  allow_blob_public_access = false
-  
+  name                            = local.name
+  location                        = var.location
+  resource_group_name             = var.resource_group_name
+  account_tier                    = var.account_tier
+  account_replication_type        = var.account_replication_type
+  min_tls_version                 = var.min_tls_version
+  allow_nested_items_to_be_public = false
+
   identity {
     type = "SystemAssigned"
   }
@@ -18,12 +18,12 @@ resource "azurerm_storage_account" "storage-account" {
     ip_rules                   = []
     virtual_network_subnet_ids = []
   }
-  
+
   blob_properties {
-    versioning_enabled = true
+    versioning_enabled       = true
     last_access_time_enabled = true
-    change_feed_enabled = true
-    
+    change_feed_enabled      = true
+
     container_delete_retention_policy {
       days = 10
     }
