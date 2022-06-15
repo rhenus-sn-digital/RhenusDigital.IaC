ebfa347cb7cdeb31115f8ecf5e871cd5afe288f9
Armin Aria <armin.aria@de.rhenus.com>
2022-06-15 10:34:15 +0200

    Added more outputs to the storage module

--- private_storage/outputs.tf
+++ private_storage/outputs.tf
@@ -2,6 +2,14 @@ output "name" {
   value = local.name
 }
 
+output "id" {
+  value = azurerm_storage_account.storage-account.id
+}
+
+output "resource_group_name" {
+  value = azurerm_storage_account.storage-account.resource_group_name
+}
+
 output "private-ip" {
   value = azurerm_private_endpoint.private-endpoint.private_service_connection[0].private_ip_address
 }
