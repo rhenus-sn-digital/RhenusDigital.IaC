36c8f979ca92251acf4272619869b6dc8adbe68d
Armin Aria <armin.aria@de.rhenus.com>
2022-07-05 11:43:45 +0200

    Fixed Secret in key vault

--- service_bus_topic/keyvault_secrets.tf
+++ service_bus_topic/keyvault_secrets.tf
@@ -22,8 +22,8 @@ resource "azurerm_key_vault_secret" "topic-mange-key" {
   value        = azurerm_servicebus_topic_authorization_rule.mange.primary_connection_string
 }
 
-resource "azurerm_key_vault_secret" "topic-id" {
+resource "azurerm_key_vault_secret" "topic-name" {
   key_vault_id = var.pipeline_key_vault_id
-  name         = "${local.name}-topic-id"
-  value        = azurerm_servicebus_topic.topic.id
+  name         = "${local.name}-topic-name"
+  value        = azurerm_servicebus_topic.topic.name
 }
