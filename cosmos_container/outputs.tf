4da5b13b45f921b372497e55f48eada663db22ee
Fabian Jeppel <fabian.jeppel@de.rhenus.com>
2022-07-11 14:37:33 +0200

    Added Connectionstring to github

--- cosmos_container/outputs.tf
+++ cosmos_container/outputs.tf
@@ -1,3 +1,8 @@
 output "connection_string" {
+  value     = "AccountEndpoint=${data.azurerm_cosmosdb_account.account.endpoint};AccountKey=${data.azurerm_cosmosdb_account.account.primary_key};"
+  sensitive = true
+
+}
+output "primary_key" {
   value = data.azurerm_cosmosdb_account.account.primary_key
 }
